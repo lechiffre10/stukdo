3c7c6524db643d70fd8cb135bf5ee51666a086bb
[01;31m[K"name"[m[K: "Sabri Helal <[01;31m[K"email"[m[K: null>
2016-10-28 16:00:09 -0500

    Add test page for bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  get 'test' => 'pages#test'
   get 'about' => 'pages#about'
 
   # Example of regular route:
